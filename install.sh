4634dceed519be63f02571a5bdd1cac4c23b16d0
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2024-12-16 17:30:44 -0600

    added echoes

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 CYME_VERSION=v2.1.1
 
+echo 'Updating repositories'
 sudo apt update
+
+echo 'Installing dependencies'
 sudo apt install ffmpeg imagemagick jackd2 jackd2-firewire qjackctl
+
+echo 'Installing tools'
 sudo apt install 7zip-rar bat btop curl duf eza fd-find fonts-firacode fzf \
     git grep kitty neo-cli p7zip-full pipx ripgrep ssh tmux tmuxp wget \
     zoxide zsh
+
+echo 'Downloading cyme...'
 curl -OL https://github.com/tuna-f1sh/cyme/releases/download/${CYME_VERSION}/cyme-${CYME_VERSION}-1-amd64.deb \
     && sudo dpkg -i cyme-${CYME_VERSION}-1-amd64.deb \
     && rm cyme-${CYME_VERSION}-1-amd64.deb
+
+echo 'Downloading fastfetch...'
 curl -OL https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.deb \
     && sudo dpkg -i fastfetch-linux-amd64.deb \
     && rm fastfetch-linux-amd64.deb
+
+echo 'Downloading Stereo Tool...'
 curl -OL https://www.stereotool.com/download/stereo_tool_gui_jack_64 \
     && sudo mv stereo_tool_gui_jack_64 /usr/local/bin/stereo-tool-jack
 sudo cp ./assets/stereo-tool.desktop /usr/share/applications/stereo-tool.desktop
